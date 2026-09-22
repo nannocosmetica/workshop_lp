b0aa7809d2e7afd38b0ce1716de72a1a8787a7d7
Matheus <andradematheus.dev@gmail.com>
2026-09-22 07:19:27 -0300

    waiting page

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,38 @@
-import AboutSection from "./components/AboutSection";
-import CardSection from "./components/CardSection";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import HeroSection from "./components/HeroSection";
-import LocationSection from "./components/LocationSection";
-import TicketSection from "./components/PricingSection";
-import { ScrollToTop } from "./components/ScrollToTop";
-import SubscriptionSection from "./components/SubscriptionSection";
-import WhoSection from "./components/WhoSection";
+// import AboutSection from "./components/AboutSection";
+// import CardSection from "./components/CardSection";
+// import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import HeroSection from "./components/HeroSection";
+// import LocationSection from "./components/LocationSection";
+// import TicketSection from "./components/PricingSection";
+// import { ScrollToTop } from "./components/ScrollToTop";
+// import SubscriptionSection from "./components/SubscriptionSection";
+// import WhoSection from "./components/WhoSection";
 
 function App() {
   return (
-    <main className="bg-black">
-      <div className="relative min-h-screen overflow-hidden bg-neutral-950 text-white">
-        {/* Background */}
-        <div
-          className="
-        absolute inset-0
-        bg-[url('/bg.jpg')]
-        bg-cover
-        bg-center
-        "
-        />
+    // <main className="bg-black">
+    //   <div className="relative min-h-screen overflow-hidden bg-neutral-950 text-white">
+    //     {/* Background */}
+    //     <div className="absolute inset-0 bg-[url('/bg.jpg')] bg-cover bg-center" />
 
-        <div className="absolute inset-0 bg-black/20" />
+    //     <div className="absolute inset-0 bg-black/20" />
 
-        <Header />
+    //     <Header />
 
-        <HeroSection />
-      </div>
-      <CardSection />
-      <AboutSection />
-      <SubscriptionSection />
-      <WhoSection />
-      <LocationSection />
-      <TicketSection />
-      <ScrollToTop />
-      <Footer />
+    //     <HeroSection />
+    //   </div>
+    //   <CardSection />
+    //   <AboutSection />
+    //   <SubscriptionSection />
+    //   <WhoSection />
+    //   <LocationSection />
+    //   <TicketSection />
+    //   <ScrollToTop />
+    //   <Footer />
+    // </main>
+    <main className="flex w-full h-screen items-center justify-center text-3xl">
+      <div>Em breve um novo workshop para você!</div>
     </main>
   );
 }
